3a11bb67f6e763ef516265a7b625993e54262260
Bohdan <mr.bogdan.99@gmail.com>
2021-01-12 00:31:51 +0200

    fixed navtab component

--- src/components/NavTab/index.tsx
+++ src/components/NavTab/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Tabs, Tab } from '@material-ui/core';
 
+
 const NavTab: React.FC<any> = () => {
 
   return (
-    <Tabs>
-      <Tab label='Home'  to='/' component={Link} />
-      <Tab label='Rooms'  to='/rooms' component={Link} />
-      <Tab label='Booking'  to='/booking' component={Link} />
+    <Tabs value={window.location.pathname}>
+      <Tab value={'/'} label='Home' to='/' component={Link} />
+      <Tab value={'/rooms'} label='Rooms' to='/rooms' component={Link} />
+      <Tab value={'/booking'} label='Booking' to='/booking' component={Link} />
     </Tabs>
   );
 }
